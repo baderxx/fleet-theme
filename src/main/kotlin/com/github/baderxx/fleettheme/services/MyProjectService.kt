26bf94c38641f7efa0289667138d26b9015a6c38
GitHub Action <action@github.com>
2024-10-03 20:55:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/baderxx/fleettheme/services/MyProjectService.kt
+++ src/main/kotlin/com/github/baderxx/fleettheme/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.baderxx.fleettheme.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.baderxx.fleettheme.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
